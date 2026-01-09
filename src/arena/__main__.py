5f2c3e6ced94fdce2e9198096f52e3f7ca50927b
Tony Lechner <tony@tony-lechner.com>
2026-01-08 20:02:11 -0600

    Remove canned logging config

--- src/arena/__main__.py
+++ src/arena/__main__.py
@@ -1,43 +1,8 @@
 from __future__ import annotations
 
-import logging
-import logging.config
-
-logger = logging.getLogger(__name__)
-
 
 def main() -> None:
-    _init_logging()
-
-
-def _init_logging() -> None:
-    logging.config.dictConfig(
-        {
-            "version": 1,
-            "formatters": {
-                "default": {
-                    "format": "%(message)s",
-                },
-            },
-            "handlers": {
-                "default": {
-                    "class": "logging.StreamHandler",
-                    "formatter": "default",
-                },
-            },
-            "root": {"level": "WARNING", "handlers": ["default"]},
-            "loggers": {
-                **{
-                    pkg: {
-                        "level": "DEBUG",
-                        "handlers": ["default"],
-                        "propagate": False,
-                    }
-                    for pkg in ("arena", "tests", "__main__")
-                }
-            },
-        }
-    )
+    pass
 
 
 if __name__ == "__main__":
